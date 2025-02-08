dd41afb8e4d62d37a7033ed306d50fcd4c6d4afc
William Brown <williambrown97@gmail.com>
2025-02-08 00:38:43 -0500

    revert base, dummy

--- verifiers/envs/base.py
+++ verifiers/envs/base.py
@@ -1,18 +1,7 @@
 from abc import ABC, abstractmethod
-import asyncio
 from typing import Any, Callable, Dict, List, Sequence
-import uuid 
 
-from vllm import AsyncLLMEngine, SamplingParams, RequestOutput
-
-async def async_llm_chat(messages: List[Dict[str, Any]],
-                         llm: AsyncLLMEngine,
-                         sampling_params: SamplingParams) -> RequestOutput:
-    tokenizer = await llm.get_tokenizer()
-    text = tokenizer.apply_chat_template(messages, tokenize=False)
-    request_id = str(uuid.uuid4())
-    gen = llm.generate(text, sampling_params=sampling_params, request_id=request_id)
-    return await anext(gen)
+from vllm import LLM, SamplingParams
 
 
 class BaseEnv(ABC):
@@ -25,5 +14,7 @@ class BaseEnv(ABC):
         pass
     
     @abstractmethod
-    def generate(self, prompts: List[List[Dict[str, Any]]], llm: Any, sampling_params: Any) -> List[Sequence[int]]:
+    def generate(self,
+                 prompts: List[List[Dict[str, Any]]], llm: LLM,
+                 sampling_params: SamplingParams) -> List[Sequence[int]]:
         pass
