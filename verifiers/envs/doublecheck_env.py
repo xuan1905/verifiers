ae8c38ba2da11f676587336b50c4a04fc0b59de9
William Brown <williambrown97@gmail.com>
2025-02-08 06:59:47 +0000

    doublecheck working finally

--- verifiers/envs/doublecheck_env.py
+++ verifiers/envs/doublecheck_env.py
@@ -1,7 +1,5 @@
 from typing import List, Callable, Dict, Any, Sequence, Tuple
-
 from verifiers.envs.base import BaseEnv
-
 from vllm import LLM, SamplingParams, RequestOutput
 
 
@@ -15,28 +13,27 @@ class DoubleCheckEnv(BaseEnv):
     def step(self,
              states: List[Dict[str, Any]],
              llm: LLM,
-             sampling_params: SamplingParams) -> List[Tuple[Dict[str, Any], RequestOutput]]:
+             sampling_params: SamplingParams) -> Tuple[List[Dict[str, Any]], List[RequestOutput]]:
         
-        outputs = llm.chat([state["messages"] for state in states], sampling_params=sampling_params) # type: ignore
+        responses = llm.chat([state["messages"] for state in states], sampling_params=sampling_params) # type: ignore
         for i, state in enumerate(states):
-            state["messages"].append({'role': 'assistant', 'content': outputs[i].outputs[0].text})
+            state["messages"].append({'role': 'assistant', 'content': responses[i].outputs[0].text})
             state["messages"].append({'role': 'user', 'content': 'Are you sure?'})
-            state["prompt_tokens"] = len(outputs[i].prompt_token_ids)
+            state["prompt_tokens"] = len(responses[i].prompt_token_ids)
 
-        outputs = llm.chat([state["messages"] for state in states], sampling_params=sampling_params) # type: ignore
-
+        responses = llm.chat([state["messages"] for state in states], sampling_params=sampling_params) # type: ignore
         for i, state in enumerate(states):
-            state["messages"].append({'role': 'assistant', 'content': outputs[i].outputs[0].text})
+            state["messages"].append({'role': 'assistant', 'content': responses[i].outputs[0].text})
             state["completed"] = True
-        return states, outputs
+        return states, responses
 
     def generate(self, prompts: List[List[Dict[str, Any]]], llm: LLM, sampling_params: SamplingParams) -> List[Sequence[int]]:
         all_completed = False
         states = [{"messages": m, "completed": False, "prompt_tokens": -1} for m in prompts]
-        outputs = [None] * len(prompts)
+        responses = []
         while not all_completed:
-            states, outputs = self.step(states, llm, sampling_params)
+            states, responses = self.step(states, llm, sampling_params)
             all_completed = all(state["completed"] for state in states)
-        all_ids = [list(output.prompt_token_ids) + list(output.outputs[0].token_ids) for output in outputs]
-        completion_ids = [all_ids[states[i]["prompt_tokens"]:] for i, output in enumerate(outputs)]
+        all_ids = [list(r.prompt_token_ids) + list(r.outputs[0].token_ids) for r in responses]
+        completion_ids = [a[states[idx]["prompt_tokens"]:] for idx, a in enumerate(all_ids)]
         return completion_ids
