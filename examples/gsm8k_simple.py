0f8f79a413d436142a013b24453cb95daab1e311
William Brown <williambrown97@gmail.com>
2025-02-10 02:17:44 +0000

    default to 1.5B

--- examples/gsm8k_simple.py
+++ examples/gsm8k_simple.py
@@ -1,13 +1,13 @@
 from trl import GRPOTrainer
 import verifiers as vf
 
-model_name = "Qwen/Qwen2.5-7B-Instruct"
+model_name = "Qwen/Qwen2.5-1.5B-Instruct"
 model, tokenizer = vf.get_model_and_tokenizer(model_name)
 
 vf_env = vf.MathEnv(dataset="gsm8k")
 dataset = vf_env.get_dataset()
 rubric = vf_env.get_rubric()
-training_args = vf.get_default_grpo_config(run_name="gsm8k_qwen7b-i", num_gpus=8)
+training_args = vf.get_default_grpo_config(run_name="gsm8k_qwen2.5-1.5b-i", num_gpus=8)
 trainer = GRPOTrainer(
     model=model,
     processing_class=tokenizer,
