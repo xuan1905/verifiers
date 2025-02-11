738db1ffbf4c1e7bc38045016b821df9c9c9ce4e
William Brown <williambrown97@gmail.com>
2025-02-10 20:47:46 -0500

    links, examples

--- examples/math_simple.py
+++ examples/math_simple.py
@@ -1,13 +1,13 @@
 from trl import GRPOTrainer
 import verifiers as vf
 
-model_name = "Qwen/Qwen2.5-1.5B-Instruct"
+model_name = "Qwen/Qwen2.5-Math-1.5B"
 model, tokenizer = vf.get_model_and_tokenizer(model_name)
 
 vf_env = vf.MathEnv(dataset="math")
 dataset = vf_env.get_dataset()
 rubric = vf_env.get_rubric()
-training_args = vf.get_default_grpo_config(run_name="math_qwen2.5-1.5b-i", num_gpus=8)
+training_args = vf.get_default_grpo_config(run_name="math_qwen2.5-math-1.5b", num_gpus=8)
 trainer = GRPOTrainer(
     model=model,
     processing_class=tokenizer,
---
+++ links.md
@@ -0,0 +1,23 @@
+# Related Link
+
+## LLM RL Training Codebases
+- TRL
+- veRL
+- Open-Instruct (Tulu)
+- OpenRLHF
+- RLHFlow
+
+
+## `verifiers` Tutorials & Examples
+- TBD
+
+## Other LLM RL Environments
+- SWE-Gym
+
+## Related Papers
+- TBD
+
+
+## Potential `Environment` Sources
+- TextArena
+- smolagents
