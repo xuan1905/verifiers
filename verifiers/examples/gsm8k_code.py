b655b921f25f68dbc8b142166b15c373d3bb6066
William Brown <williambrown97@gmail.com>
2025-03-02 01:42:34 +0000

    examples, dependencies

--- verifiers/examples/gsm8k_code.py
+++ verifiers/examples/gsm8k_code.py
@@ -6,13 +6,16 @@ model, tokenizer = vf.get_model_and_tokenizer(model_name)
 
 vf_env = vf.CodeEnv(dataset="gsm8k", few_shot=[], system_prompt=CODE_PROMPT)
 dataset = vf_env.get_dataset()
+eval_dataset = vf_env.get_eval_dataset(n=100)
 rubric = vf_env.get_rubric()
+
+# notable defaults: lr = 1e-6, max_grad_norm = 0.01, constant lr 10 warmup steps, 1024 tokens in+out
 training_args = vf.get_default_grpo_config(run_name="gsm8k_qwen2.5-7b", num_gpus=8)
 # rollouts per prompt
 training_args.num_generations = 7
-# minibatch size per GPU ( bs 6 * 7 gpus / 7 rollouts -> 6 prompts per batch)
-training_args.per_device_train_batch_size = 6
-# batches to accumulate (6 prompts * 4 -> 24 prompts per global batch)
+# minibatch size per GPU ( bs 6 * 7 gpus / 7 rollouts -> 8 prompts per batch)
+training_args.per_device_train_batch_size = 8
+# batches to accumulate (8 prompts * 4 -> 32 prompts per global batch)
 training_args.gradient_accumulation_steps = 4
 # steps per global batch (1 on-policy, 1 off-policy)
 training_args.num_iterations = 2
@@ -22,6 +25,7 @@ training_args.beta = 0.0
 training_args.eval_strategy = "steps"
 training_args.eval_steps = 100
 training_args.eval_accumulation_steps = 8
+
 trainer = vf.GRPOEnvTrainer(
     model=model,
     processing_class=tokenizer,
@@ -29,5 +33,6 @@ trainer = vf.GRPOEnvTrainer(
     env=vf_env,
     args=training_args,
     train_dataset=dataset,
+    eval_dataset=eval_dataset,
 )
 trainer.train()
--- verifiers/examples/math_code.py
+++ verifiers/examples/math_code.py
@@ -13,15 +13,14 @@ rubric = vf_env.get_rubric()
 training_args = vf.get_default_grpo_config(run_name="math_qwen2.5-7b", num_gpus=8)
 # rollouts per prompt
 training_args.num_generations = 7
-# minibatch size per GPU ( bs 6 * 7 gpus / 7 rollouts -> 6 prompts per batch)
-training_args.per_device_train_batch_size = 6
-# batches to accumulate (6 prompts * 4 -> 24 prompts per global batch)
+# minibatch size per GPU ( bs 8 * 7 gpus / 7 rollouts -> 8 prompts per batch)
+training_args.per_device_train_batch_size = 8
+# batches to accumulate (8 prompts * 4 -> 32 prompts per global batch)
 training_args.gradient_accumulation_steps = 4
 # steps per global batch (1 on-policy, 1 off-policy)
 training_args.num_iterations = 2
 # no ref model
 training_args.beta = 0.0
-
 # evals
 training_args.eval_strategy = "steps"
 training_args.eval_steps = 100
